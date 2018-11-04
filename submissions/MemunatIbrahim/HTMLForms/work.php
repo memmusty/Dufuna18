dac500278dd51acbfa23193e6199bc952edc64f4
Memunat Ibrahim <memunati@gmail.com>
2018-11-04 13:34:00 +0100

    record registered members details into the database

---
+++ submissions/MemunatIbrahim/HTMLForms/work.php
@@ -0,0 +1,27 @@
+<?php
+if(isset($_POST['submit'])){
+    $fname=$_POST['fname'];
+    $lname=$_POST['lname'];
+    $mail=$_POST['mail'];
+    $pword=$_POST['pword'];
+    $phone=$_POST['phone'];
+    $gender=$_POST['gender'];
+    $country=$_POST['country'];
+    $conn = mysqli_connect('localhost', 'user', '1234', 'registration');
+    // Check that connection exists
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    $sql = "INSERT INTO members VALUES (null, '$fname', '$lname', '$mail', '$pword','$phone','$gender','$country',null)";
+    $result = mysqli_query($conn, $sql);
+    //Check for errors
+    if (!$result) {
+        die("Error: " . $sql . "<br>" . mysqli_error($conn));
+    }
+    else{        
+     header('Location:success.html');
+    }
+    //Close the connection
+    mysqli_close($conn);
+}
+?>
