70481161de6735657f856029025e9ca6f2d5097f
Memunat Ibrahim <memunati@gmail.com>
2018-12-14 00:09:16 +0100

    resolved navbar issues

--- submissions/MemunatIbrahim/FoodDey/resolve.php
+++ submissions/MemunatIbrahim/FoodDey/resolve.php
@@ -31,7 +31,7 @@
 
                             <!-- Collect the nav links, forms, and other content for toggling -->
                             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                              <ul class="nav navbar-nav navbar-right" id="top-nav">
+                              <ul class="nav navbar-nav navbar-right">
                                 <li><a href="./home.php">Home</a></li>
                                 <?
                                     //check if user is logged in
